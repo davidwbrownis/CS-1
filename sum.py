83493b1af961e98760b20a7f18bef3183afacfcb
davidwbrownis <32481666+davidwbrownis@users.noreply.github.com>
2017-10-11 21:43:42 +1100

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,11 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
